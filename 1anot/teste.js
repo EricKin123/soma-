db765858127a3b8dcef9e8f1268f6d651cb50f04
Eric <163418429+EricKin123@users.noreply.github.com>
2024-11-07 09:57:35 -0300

    Add files via upload

---
+++ 1anot/teste.js
@@ -0,0 +1,6 @@
+let numero1= parseInt(prompt("digite o primeiro numero: "));
+let numero2= parseInt(prompt("digite o segundo numero: "));
+let soma = numero1+ numero2;
+document.white("A soma é igual a: ", soma );
+console.log("O valor da soma no console será igual a: ", soma);
+
